a41019a1efdf5dfa324c43c9e2756a7ca0027906
aymericvanderkelen <aymeric62vanderkelen@gmail.com>
2020-07-04 14:16:00 +0200

    initial commit

---
+++ src/main/java/com/decathlon/movie_api/MovieApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.decathlon.movie_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieApiRestApplication.class, args);
+    }
+
+}
